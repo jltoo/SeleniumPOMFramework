1103c499950f83a2aa7d2f12c610700cf4bc8a73
Lovie <jlovietoo@gmail.com>
2022-09-17 17:16:24 +0800

    fixed testng.xml

--- src/main/java/test/TestSearchGoogle.java
+++ src/main/java/test/TestSearchGoogle.java
@@ -14,19 +14,19 @@ public class TestSearchGoogle extends BaseTest {
 	HomePage homePage;
 	String tcName = "Reg_TC_001_Verify_User_Can_Search";
 
-	@Test
+	@Test(priority= 1)
 	public void openHomePage() {
 		setUpTest(tcName, "This is a Sample Test");
 		openUrl("https://www.google.com");	
 	}
 	
-	@Test
+	@Test(priority= 2)
 	public void testGoogleSearch() {
 		homePage = new HomePage(driver);
 		homePage.searchFromGoogle("test");
 	}
 	
-	@Test
+	@Test(priority= 3)
 	public void endTest() {
 		closeBrowser();
 		
--- src/main/java/test/TestSearchGoogle2.java
+++ src/main/java/test/TestSearchGoogle2.java
@@ -8,19 +8,19 @@ public class TestSearchGoogle2 extends BaseTest {
 	HomePage homePage;
 	String tcName = "Reg_TC_002_Verify_User_Can_Search";
 
-	@Test(priority=1)
+	@Test(priority= 1)
 	public void openHomePage() {
 		setUpTest(tcName, "This is a Sample Test");
 		openUrl("https://www.google.com");	
 	}
 	
-	@Test (priority=2)
-	public void atestGoogleSearch() {
+	@Test(priority= 2)
+	public void testGoogleSearch() {
 		homePage = new HomePage(driver);
 		homePage.searchFromGoogle("sample");
 	}
 	
-	@Test(priority=3)
+	@Test(priority= 3)
 	public void endTest() {
 		closeBrowser();
 		
